2127ab2554e6c28fd3634f1a20116eb4c1112de1
vonSchalienR <vonschalienrosa@gmail.com>
2024-04-11 17:19:18 +0300

    Update swisteht.java

--- SWIS/swisteht.java
+++ SWIS/swisteht.java
@@ -1,6 +1,24 @@
-package 
+package SWIS;
+
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+
+public class swisteht {
+
+    public static void main(String[] args) {
+        System.out.println("It is currently: " + getCurrentTime("America/New_York") + "in New York, United States. ");
+    }
+
+    private static String getCurrentTime(String zoneId) {
+        final ZoneId locality = ZoneId.of(zoneId);
+        final ZonedDateTime now = ZonedDateTime.now(locality);
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a E, dd MMMM uuuu");
+        return formatter.format(now);
+    }
+    
+}
+
+
 
 
